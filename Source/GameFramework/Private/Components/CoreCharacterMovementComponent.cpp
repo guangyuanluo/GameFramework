c1b429a9c907aa1023ca38a855b5880bb7298263
guangyuanluo <869192994@qq.com>
2025-04-15 20:19:29 +0800

    客户端本地PushAway避免卡死

--- Source/GameFramework/Private/Components/CoreCharacterMovementComponent.cpp
+++ Source/GameFramework/Private/Components/CoreCharacterMovementComponent.cpp
@@ -1,8 +1,17 @@
 #include "Components/CoreCharacterMovementComponent.h"
 #include "Character/CoreCharacter.h"
+#include "GameFramework/Character.h"
+#include "Components/CapsuleComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/KismetMathLibrary.h"
 
 UCoreCharacterMovementComponent::UCoreCharacterMovementComponent(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer) {
+	
+}
+
+void UCoreCharacterMovementComponent::SetCanBePush(bool bEnable) {
+	bCanBePushed = bEnable;
 }
 
 bool UCoreCharacterMovementComponent::ResolvePenetrationImpl(const FVector & Adjustment, const FHitResult & Hit, const FQuat & NewRotation) {
@@ -11,4 +20,67 @@ bool UCoreCharacterMovementComponent::ResolvePenetrationImpl(const FVector & Adj
 		return false;
 	}
 	return Super::ResolvePenetrationImpl(Adjustment, Hit, NewRotation);
+}
+
+void UCoreCharacterMovementComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) {
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	TestAndPushOther(DeltaTime);
+}
+
+void UCoreCharacterMovementComponent::TestAndPushOther(const float DeltaTime) {
+	if (!bCanBePushed) {
+		return;
+	}
+	if (!CharacterOwner->IsLocallyControlled() && !CharacterOwner->HasAuthority()) {
+		return;
+	}
+	if (!IsMovingOnGround()) {
+		return;
+	}
+	//播动画过程不push
+	if (CharacterOwner->IsPlayingRootMotion()) {
+		return;
+	}
+	if (TestPushIgnoreActors.Num() == 0) {
+		TestPushIgnoreActors.Add(CharacterOwner);
+	}
+
+	const bool HaveFind = UKismetSystemLibrary::CapsuleOverlapComponents(
+		this,
+		CharacterOwner->GetActorLocation(),
+		CharacterOwner->GetCapsuleComponent()->GetScaledCapsuleRadius() + 30.0f + ThresholdDistanceBias,
+		CharacterOwner->GetCapsuleComponent()->GetScaledCapsuleHalfHeight(),
+		TestPushObjectTypes,
+		UCapsuleComponent::StaticClass(),
+		TestPushIgnoreActors,
+		TestPushOverlapComponents
+	);
+	if (!HaveFind) {
+		return;
+	}
+	for (const UPrimitiveComponent* OtherComponent : TestPushOverlapComponents)
+	{
+		ACharacter* OtherCharacter = Cast<ACharacter>(OtherComponent->GetOwner());
+		if (OtherCharacter && OtherCharacter != CharacterOwner) {
+			PushAwayOther(DeltaTime, OtherCharacter);
+		}
+	}
+	TestPushOverlapComponents.Empty();
+}
+
+void UCoreCharacterMovementComponent::PushAwayOther(const float DeltaTime, class ACharacter* OtherCharacter) {
+	const float TargetPosBias = CharacterOwner->GetCapsuleComponent()->GetScaledCapsuleRadius() + OtherCharacter->GetCapsuleComponent()->GetScaledCapsuleRadius() + 30.f + ThresholdDistanceBias;
+	FVector PosBias = CharacterOwner->GetMesh()->GetComponentLocation() - OtherCharacter->GetMesh()->GetComponentLocation();
+	if (FMath::Abs(PosBias.Z) > CharacterOwner->GetCapsuleComponent()->GetScaledCapsuleHalfHeight())
+	{
+		return;
+	}
+	PosBias.Z = 0;
+	if (!PosBias.IsNearlyZero() && PosBias.SizeSquared() < FMath::Square(TargetPosBias - 0.01f))
+	{
+		FHitResult OutSweepHit;
+		FVector AdjustOffset = PosBias.GetSafeNormal() * FMath::Min(TargetPosBias - PosBias.Size(), TestPushLength);
+		UpdatedComponent->AddWorldOffset(AdjustOffset, true, &OutSweepHit);
+	}
 }
--- Source/GameFramework/Public/Components/CoreCharacterMovementComponent.h
+++ Source/GameFramework/Public/Components/CoreCharacterMovementComponent.h
@@ -14,5 +14,30 @@ class GAMEFRAMEWORK_API UCoreCharacterMovementComponent : public UCharacterMovem
 	GENERATED_UCLASS_BODY()
 
 public:
+	UPROPERTY(EditDefaultsOnly)
+	float ThresholdDistanceBias = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly)
+	TArray<TEnumAsByte<EObjectTypeQuery>> TestPushObjectTypes;
+
+	UPROPERTY(EditDefaultsOnly)
+	float TestPushLength = 5.f;
+
+	UFUNCTION(BlueprintCallable)
+	void SetCanBePush(bool bEnable);
+
 	virtual bool ResolvePenetrationImpl(const FVector& Adjustment, const FHitResult& Hit, const FQuat& NewRotation) override;
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+protected:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	bool bCanBePushed = false;
+
+private:
+	TArray<UPrimitiveComponent*> TestPushOverlapComponents;
+	TArray<AActor*> TestPushIgnoreActors;
+
+	//防止播动画过程中胶囊体互相嵌入卡死
+	virtual void TestAndPushOther(const float DeltaTime);
+	virtual void PushAwayOther(const float DeltaTime, class ACharacter* OtherCharacter);
 };
